998935204585872f1ad9d363f09dddfce5deeb35
SamuelHFS <samuel.jpg1@gmail.com>
2021-10-17 18:05:43 -0300

    tudo funcionando

---
+++ pjWebPage/pjWebPage/pjWebPage/model/BaseUrlInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pjWebPage.model
+{
+   class BaseUrlInterface : IBaseUrl
+    {
+        public string Get()
+        {
+            return "file:///android_asset/Index.html";
+        }
+    }
+}
+
+
+
+
---
+++ pjWebPage/pjWebPage/pjWebPage/model/IBaseUrl.cs
@@ -0,0 +1,16 @@
+﻿using pjWebPage.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+[assembly: Xamarin.Forms.Dependency(typeof(BaseUrlInterface))]
+namespace pjWebPage.model
+{
+    interface IBaseUrl
+    {
+        string Get();
+    }
+}
